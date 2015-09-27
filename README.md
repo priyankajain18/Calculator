ff0c0dea7cb4fd9e55caab18115182e115ba7397
Priyanka Jain <priyanka.jain@fulfil.io>
2015-09-27 14:25:15 +0530

    Basic Calculator for Mathematical Operations

---
+++ README.md
@@ -0,0 +1,18 @@
+#Calculator
+*Basic Calculator for Mathematical Operations*
+
+###Installation:
+
+* Clone the Repository.
+* cd Calculator
+* wget https://pypi.python.org/packages/source/S/SimpleGUITk/SimpleGUITk-1.1.3.tar.gz --no-check-certificate
+* tar -xvzf SimpleGUITk-1.1.3.tar.gz
+* cd SimpleGUITk-1.1.3
+* chmod 755 setup.py
+* ./setup.py -v build
+* ./setup.py -v install
+* Open calculator.py file under this directory.
+* Run python calculator.py
+
+###Screenshot:
+![calculator](https://github.com/priyankajain18/Calculator/blob/master/SimpleGUITk-1.1.3/Screenshot/screenshot.png)
--- SimpleGUITk-1.1.3/calculator.py
+++ SimpleGUITk-1.1.3/calculator.py
@@ -1,10 +1,11 @@
 import simpleguitk as simplegui
 
 
+#Initialize Global Variables
 variable1 = 0;
 variable2 = 0;
 
-def input():
+def input_output():
     print "Input ----->";
     print "Number 1: ", variable1;
     print "Number 2: ", variable2;
@@ -18,42 +19,55 @@ def input2(input2):
     global variable2;
     variable2 = float(input2);
 
+#Division Function
 def divide():
-    input();
-    print "Result: ", (variable1/variable2);
-    return (variable1/variable2);
+    if variable2 != 0:
+        input_output();
+        print "Result: ", (variable1/variable2);
+        return (variable1/variable2);
+    else:
+        print "Zero Division Error";
+        return "Zero Division Error";
 
+#Multiplication Function
 def multiply():
-    input();
+    input_output()
     print "Result: ", (variable1 * variable2);
     return (variable1 * variable2);
 
+#Addition Function
 def add():
-    input();
+    input_output()
     print "Result: ", (variable1 + variable2);
     return (variable1 + variable2);
 
+#Subtraction Function
 def subtract():
-    input();
+    input_output()
     print "Result: ", (variable1 - variable2);
     return (variable1 - variable2);
 
+#Swap Function
 def swap():
-    input();
+    input_output()
     variable3, variable4 = variable2, variable1;
     print "Number 1: ", variable3;
     print "Number 2: ", variable4;
     return variable3, variable4;
 
+#Creating Frame
 frame = simplegui.create_frame("Calculator", 500, 500);
 
+#Creating Input Fields
 frame.add_input("Enter 1st Number", input1, 50);
 frame.add_input("Enter 2nd Number", input2, 50);
 
+#Creating Buttons
 frame.add_button("Divide", divide, 50);
 frame.add_button("Multiply", multiply, 50);
 frame.add_button("Add", add, 50);
 frame.add_button("Subtract", subtract, 50);
 frame.add_button("Swap", swap, 50);
 
+#Initializing Frame
 frame.start();
